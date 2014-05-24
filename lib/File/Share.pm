564a8045564fd7d6cf9a17b30787758e0c858860
Ingy döt Net <ingy@ingy.net>
2014-05-23 18:37:24 -0700

    version 0.10

--- lib/File/Share.pm
+++ lib/File/Share.pm
@@ -1,19 +1,9 @@
-##
-# name:      File::Share
-# abstract:  Extend File::ShareDir to Local Libraries
-# author:    Ingy döt Net <ingy@ingy.net>
-# license:   perl
-# copyright: 2011, 2013
-# see:
-# - File::ShareDir
-# - Devel::Local
+# ABSTRACT: Extend File::ShareDir to Local Libraries
 
-use v5.8.3;
 package File::Share;
+
 use strict;
 use warnings;
-
-our $VERSION = '0.03';
 
 use base 'Exporter';
 our @EXPORT_OK   = qw[
@@ -40,7 +30,7 @@ sub dist_dir {
     my $path = $INC{$inc} || '';
     if ($path and
         $path =~ s!(\S.*?)[\\/]?\bb?lib\b.*!$1! and
-        ( -e "$path/Makefile.PL" or -e "$path/dist.ini" or -e "$path/Build.PL" ) and
+        -d "$path/lib" and
         -e "$path/share"
     ) {
         return abs_path "$path/share";
@@ -64,35 +54,4 @@ sub module_file {
     die "File::Share::module_file not yet supported";
 }
 
-=head1 SYNOPSIS
-
-    use File::Share ':all';
-
-    my $dir = dist_dir('Foo-Bar');
-    my $file = dist_file('Foo-Bar', 'file.txt');
-
-=head1 DESCRIPTION
-
-THis module is a dropin replacement for L<File::ShareDir>. It supports the
-C<dist_dir> and C<dist_file> functions, except these functions have been
-enhanced to understand when the developer's local C<./share/> directory should
-be used.
-
-NOTE: module_dist and module_file are not yet supported, because (afaik) there
-is no well known way to populate per-module share files. This may change in
-the future. Please contact me if you know how to do this.
-
-=head1 PROBLEM AND SOLUTION
-
-L<Module::Install> has an C<install_share> directive that allows you to
-install various files associated with a distribution. By convention, module
-authors always put these in a directory called C<share/>. However,
-File::ShareDir can only find files after they have been installed. This can be
-problematic when running development tests.
-
-File::Share will look for a local C<share> directory, if it notices that the
-module corresponding was loaded from a development path.
-
-L<Devel::Local> gives you an easy way to use a bunch of source repositories as
-though their lib and bin directories had already been installed.
-C<File::Share> lets you play along with that.
+1;
